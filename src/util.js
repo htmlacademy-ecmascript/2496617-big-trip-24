c3dc5d5275cb3c90161fb1f5724626f5bc435fac
alexx-kis <kislov@advline.ru>
2024-09-06 11:39:54 +0300

    import path fixed

--- src/util.js
+++ src/util.js
@@ -1,5 +1,5 @@
 import dayjs from 'dayjs';
-import { DATE_FORMAT, TIME_FORMAT, DATE_AND_TIME_FORMAT } from '';
+import { DATE_FORMAT, TIME_FORMAT, DATE_AND_TIME_FORMAT } from './const';
 
 // $======================== util ========================$ //
 
