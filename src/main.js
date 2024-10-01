67cbea47bafcb70878c0d765cb004d6205e86697
alexx-kis <kislov@advline.ru>
2024-10-01 15:03:31 +0300

    + FilterModel

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import PointsModel from './model/points-model';
 import DestinationsModel from './model/destinations-model';
 import OffersModel from './model/offers-model';
+import FilterModel from './model/filter-model';
 
 import MainPresenter from './presenter/main-presenter';
 
@@ -11,6 +12,7 @@ const filtersElement = document.querySelector('.trip-controls__filters');
 const pointsModel = new PointsModel();
 const offersModel = new OffersModel();
 const destinationsModel = new DestinationsModel();
+const filterModel = new FilterModel();
 
 const mainPresenter = new MainPresenter({
   pointsContainer: pointsElement,
---
+++ src/model/filter-model.js
@@ -0,0 +1,17 @@
+import Observable from '../framework/observable';
+import { FilterType } from '../utils/filter';
+
+// $======================== FilterModel ========================$ //
+
+export default class FilterModel extends Observable {
+  #filter = FilterType.EVERYTHING;
+
+  get filter() {
+    return this.#filter;
+  }
+
+  setFilter(updateType, filter) {
+    this.#filter = filter;
+    this._notify(updateType, filter);
+  }
+}
